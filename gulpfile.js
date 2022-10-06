2b3b1a19561de8251baf8231a2a82a6f0cb7c7d9
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-10-06 16:47:23 +0200

    Remove the sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,20 @@
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
 const rename = require('gulp-rename');
-const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
-gulp.task('eslint', function () {
+exports.eslint = function () {
     return gulp.src(['src/juggler.js'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-});
+};
 
-gulp.task('default', function () {
+exports.default = function () {
     return gulp.src('src/juggler.js')
-        .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(rename({dirname: ''}))
         .pipe(gulp.dest('dist'))
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
-        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
-});
+};
